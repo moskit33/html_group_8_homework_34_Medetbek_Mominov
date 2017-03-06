e409c69207162ac6dee6cd88cd8ab616dc6fdd3d
moskit <moskit544@gmail.com>
2017-03-06 15:38:22 +0600

    Initial files

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $(".navigation a").on("click", function(event) {
+        event.preventDefault();
+        var id  = $(this).attr('href');
+
+        var top = $(id).offset().top;
+
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+
+$('.popup-btn').click(function () {
+    $('.popup-wrap').fadeIn(1000);
+});
+
+$('.popup-close').click(function () {
+    $('.popup-wrap').fadeOut(1000);
+});
+
+$('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000
+});
+
